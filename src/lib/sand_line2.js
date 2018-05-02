647870e3ae54f91702bbfc6dd7c78ce1accf57c2
ajfisher <ajfisher.td@gmail.com>
2018-05-02 22:58:04 +1000

    Mods to refactor the sandline drawing to prep for rAF

--- src/lib/sand_line2.js
+++ src/lib/sand_line2.js
@@ -6,6 +6,71 @@ import simplex_noise from 'simplex-noise';
 
 import { best_contrast, rnd_range } from './utils';
 
+class SandPoint {
+    // used to determine a part of a sandline associated to a specific
+    // point along the line.
+
+    constructor (x, y, colour, ctx, ...opts) {
+
+        this.x = x || 0;
+        this.y = y || 0;
+        this.colour = colour || "#ffffff";
+    }
+
+    draw (ctx) {
+
+        ctx.fillStyle = this.colour;
+
+        ctx.save();
+        ctx.globalAlpha = 0.01;
+        ctx.translate(this.x, this.y);
+        ctx.fillRect(0, 0, 4, 4);
+        ctx.restore();
+    }
+
+}
+
+class SandPass {
+    // this class is used for doing a pass of a particular sand line.
+
+    constructor(x1, y1, x2, y2, pass_points, v_points=100, volatility=700) {
+
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.pass_points = pass_points;
+        this.v_points = v_points;
+        this.y_volatility = volatility;
+
+        // determine the number of points along the x-axis needed.
+        this.x_incr = (this.x2 - this.x1) / this.pass_points;
+    }
+
+    draw (ctx, colour) {
+
+        // walk the line
+        for (let x = this.x1; x < this.x2; x = x + this.x_incr) {
+
+            const cx = x + rnd_range(-5, 5);
+            const ny1 = rnd_range(this.y1, this.y1 - this.y_volatility);
+            const ny2 = rnd_range(this.y1, this.y1 + this.y_volatility);
+
+            // now work out how many y points to plot by dividing it up.
+            const y_incr = (ny2 - ny1) / this.v_points;
+
+            ctx.save()
+            for (let p = 0; p < this.v_points; p++) {
+                const y = ny1 + (p * y_incr);
+                new SandPoint(x, y, colour).draw(ctx);
+            }
+            ctx.restore();
+        }
+
+    }
+
+}
+
 export default class SandLines {
 
     constructor (options) {
@@ -23,54 +88,21 @@ export default class SandLines {
         this.canvas = opts.canvas;
         this.palettes = opts.palettes;
         this.lines = 4;
+        this.draw_queue = [];
     }
 
-    pixel(ctx, x, y, colour) {
-
-        ctx.fillStyle = colour;
-
-        ctx.save();
-        ctx.globalAlpha = 0.01;
-        ctx.translate(x, y);
-        ctx.fillRect(0, 0, 4, 4);
-        ctx.restore();
-    }
-
-
-    line(ctx, x1, y1, x2, y2, colour, points, passes, line_points, volatility) {
-
-        //const simplex = new simplex_noise(Math.random);
-
-        // get a number of points or use all of them
-        const pts = points || 100;
-        const y_volatility = volatility || 700;
-        const x_incr = (x2 - x1) / line_points;
-
-        //ctx.strokeStyle = colour;
-        //ctx.lineWidth = 2;
+    line(ctx, x1, y1, x2, y2, colour,
+        grains=100, passes, pass_points, y_volatility=100) {
+        // creates a line between x1,y1 and x2,y2
+        // passes establishes the number of times you need to go over the work
+        // grains is the number of grains that are droped at each point along
+        // the line. 
+        // pass points is the number of points looked at during each pass.
 
         for (let pass_no = 0; pass_no < passes; pass_no++) {
 
-
-            // walk the line
-            for (let x = x1; x < x2; x = x + x_incr) {
-
-                const cx = x + rnd_range(-5, 5);
-                const ny1 = rnd_range(y1, y1 - y_volatility);
-                const ny2 = rnd_range(y1, y1 + y_volatility);
-
-                // now work out how many y points to plot by dividing it up.
-                const y_incr = (ny2 - ny1) / pts;
-
-                window.requestAnimationFrame(() => {
-                    ctx.save()
-                    for (let p = 0; p < pts; p++) {
-                        const y = ny1 + (p * y_incr);
-                        this.pixel(ctx, cx, y, colour);
-                    }
-                    ctx.restore();
-                });
-            }
+            new SandPass(x1, y1, x2, y2, pass_points, grains, y_volatility)
+                .draw(ctx, colour);
         }
     }
 
@@ -96,7 +128,20 @@ export default class SandLines {
         ctx.restore();
     }
 
-    draw (seed) {
+    process () {
+        // does the processing of the draw queue 
+
+    }
+
+
+    draw (seed, options) {
+        // set off the drawing process.
+        // `seed` provides a random seed as an `int` to use for recreation
+        // `options` is an object
+        // `neutral` is a `boolean` which if set determines whether to use
+        // a palette (false or undef) or the black and white palette (true)
+
+        const opts = options || {};
 
         this.seed = parseInt(seed) || Math.floor(Math.random() * (Math.pow(2,20)));
         Math.seedrandom(this.seed);
@@ -111,6 +156,11 @@ export default class SandLines {
 
         let ctx = this.canvas.getContext('2d');
         let palette = arrayShuffle(this.palettes)[0];
+
+        if (typeof(opts.neutral) != 'undefined' && opts.neutral) {
+            palette = this.palettes[0];
+        }
+
         let bg = palette[0];
         let line_colour = palette[best_contrast(palette, bg)];
         //console.log(palette, bg, line_colour);
@@ -118,8 +168,11 @@ export default class SandLines {
         ctx.fillStyle = bg;
         ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-        const no_lines = 3;
-        const passes = 100;
+        // put the seed on the bottom
+        this.text(ctx, this.seed, bg, line_colour);
+
+        const no_lines = 1;
+        const passes = 50;
         const grains = 80;
         const line_points = this.canvas.width / 10;
         const volatility = this.canvas.height / (no_lines + 1) * 0.7;
@@ -128,12 +181,13 @@ export default class SandLines {
 
             const y = this.canvas.height / (no_lines + 1) * line;
 
-        this.line(ctx, 0, y, this.canvas.width, y, line_colour,
-                    grains, passes, line_points, volatility);
+            // push this onto a queue for processing.
+            this.line(ctx, 0, y, this.canvas.width, y, line_colour,
+                grains, passes, line_points, volatility);
         }
 
-        // put the seed on the bottom
-        this.text(ctx, this.seed, bg, line_colour);
+        // now kick off the queue process.
+        //window.requestAnimationFrame(() => this.process);
     }
 }
 
