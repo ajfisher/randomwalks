4d8b63c4b3ec2b20d94914998d6a69f445f52b91
ajfisher <ajfisher.td@gmail.com>
2018-05-05 13:21:47 +1000

    refactor to break up line drawing method

--- src/lib/sand_line2.js
+++ src/lib/sand_line2.js
@@ -7,14 +7,14 @@ import simplex_noise from 'simplex-noise';
 import { best_contrast, rnd_range } from './utils';
 
 class SandPoint {
-    // used to determine a part of a sandline associated to a specific
-    // point along the line.
+    // used to plot a specific point at x, y
 
-    constructor (x, y, colour, ctx, ...opts) {
+    constructor (x=0, y=0, colour="#ffffff", ...opts) {
 
-        this.x = x || 0;
-        this.y = y || 0;
-        this.colour = colour || "#ffffff";
+        this.x = x;
+        this.y = y;
+        this.colour = colour;
+        this.alpha = opts[0] || 0.01;
     }
 
     draw (ctx) {
@@ -22,7 +22,7 @@ class SandPoint {
         ctx.fillStyle = this.colour;
 
         ctx.save();
-        ctx.globalAlpha = 0.01;
+        ctx.globalAlpha = this.alpha;
         ctx.translate(this.x, this.y);
         ctx.fillRect(0, 0, 4, 4);
         ctx.restore();
@@ -30,45 +30,71 @@ class SandPoint {
 
 }
 
-class SandPass {
-    // this class is used for doing a pass of a particular sand line.
+class SandLine {
+    // used to draw a straight line from one place to another
+    constructor (x1=0, y1=0, x2=0, y2=0, points, colour="#ffffff", ...opts) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.points = points;
+        this.colour = colour;
 
-    constructor(x1, y1, x2, y2, pass_points, v_points=100, volatility=700) {
+        this.alpha = opts[0] || 0.01;
+    }
+
+    draw(ctx) {
+        // draws a line from (x1, y1) to (x2, y2) using the number of points
+        // along the line to draw it.
+
+        // TODO this only draws a vertical line and it should go between
+        // the two points properly. This needs to get fixed.
+
+        // based on the length of the perpendicular line, determine
+        // the gaps between the points as there is always a fixed number
+        // of points.
+        const y_incr = (this.y2 - this.y1) / this.points;
+
+        for (let p = 0; p < this.points; p++) {
+            const y = this.y1 + (p * y_incr);
+            new SandPoint(this.x1, y, this.colour, this.alpha).draw(ctx);
+        }
+    }
+}
+
+class SandPass {
+    // this class is used for doing a pass on a particular path.
+
+    constructor(x1, y1, x2, y2, path_points, line_points=100, volatility=700) {
 
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
-        this.pass_points = pass_points;
-        this.v_points = v_points;
+        this.path_points = path_points;
+        this.line_points = line_points;
         this.y_volatility = volatility;
 
         // determine the number of points along the x-axis needed.
-        this.x_incr = (this.x2 - this.x1) / this.pass_points;
+        this.x_incr = (this.x2 - this.x1) / this.path_points;
     }
 
     draw (ctx, colour) {
 
-        // walk the line
+        // walk the line and for each point on `x_incr` create a sandline
+        // perpendicular to the direction of the path.
         for (let x = this.x1; x < this.x2; x = x + this.x_incr) {
 
-            const cx = x + rnd_range(-5, 5);
+            // jitter the x coordinate around the incremental point
+            const cx = x + rnd_range(-3, 3);
+            // choose points for y which come off the base y point ±
+            // the amount of volatility
             const ny1 = rnd_range(this.y1, this.y1 - this.y_volatility);
             const ny2 = rnd_range(this.y1, this.y1 + this.y_volatility);
 
-            // now work out how many y points to plot by dividing it up.
-            const y_incr = (ny2 - ny1) / this.v_points;
-
-            ctx.save()
-            for (let p = 0; p < this.v_points; p++) {
-                const y = ny1 + (p * y_incr);
-                new SandPoint(x, y, colour).draw(ctx);
-            }
-            ctx.restore();
+            new SandLine(cx, ny1, cx, ny2, this.line_points, colour).draw(ctx);
         }
-
     }
-
 }
 
 export default class SandLines {
@@ -91,17 +117,17 @@ export default class SandLines {
         this.draw_queue = [];
     }
 
-    line(ctx, x1, y1, x2, y2, colour,
-        grains=100, passes, pass_points, y_volatility=100) {
+    path(ctx, x1, y1, x2, y2, colour,
+        grains=100, passes, path_points, y_volatility=100) {
         // creates a line between x1,y1 and x2,y2
         // passes establishes the number of times you need to go over the work
         // grains is the number of grains that are droped at each point along
-        // the line. 
+        // the line.
         // pass points is the number of points looked at during each pass.
 
         for (let pass_no = 0; pass_no < passes; pass_no++) {
 
-            new SandPass(x1, y1, x2, y2, pass_points, grains, y_volatility)
+            new SandPass(x1, y1, x2, y2, path_points, grains, y_volatility)
                 .draw(ctx, colour);
         }
     }
@@ -174,7 +200,7 @@ export default class SandLines {
         const no_lines = 1;
         const passes = 50;
         const grains = 80;
-        const line_points = this.canvas.width / 10;
+        const path_points = this.canvas.width / 10;
         const volatility = this.canvas.height / (no_lines + 1) * 0.7;
 
         for (let line = 1; line <= no_lines; line++) {
@@ -182,8 +208,8 @@ export default class SandLines {
             const y = this.canvas.height / (no_lines + 1) * line;
 
             // push this onto a queue for processing.
-            this.line(ctx, 0, y, this.canvas.width, y, line_colour,
-                grains, passes, line_points, volatility);
+            this.path(ctx, 0, y, this.canvas.width, y, line_colour,
+                grains, passes, path_points, volatility);
         }
 
         // now kick off the queue process.
