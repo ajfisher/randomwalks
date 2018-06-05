5ded3172c7c0204ca97a2d562543a084d3fe2a85
ajfisher <ajfisher.td@gmail.com>
2018-06-05 22:07:25 +1000

    Linter fixes for app

--- src/app.js
+++ src/app.js
@@ -17,64 +17,62 @@ let random_arcs = null;
 let sand_lines = null;
 let poly = null;
 
-function convert(palettes) {
-    // goes through all of the palettes and converts each one to HSV
-    // colour space to allow easier manipulation
+function convert(palette_list) {
+  // goes through all of the palettes and converts each one to HSV
+  // colour space to allow easier manipulation
 
-    return palettes.map((palette) => {
-        return palette.map((colour) => {
+  return palette_list.map((palette) => {
+    return palette.map((colour) => {
+      let rgb = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(colour);
+      rgb = rgb ? [
+        parseInt(rgb[1], 16), parseInt(rgb[2], 16), parseInt(rgb[3], 16)
+      ] : null;
 
-            let rgb = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(colour);
-            rgb = rgb ? [
-                parseInt(rgb[1], 16), parseInt(rgb[2], 16), parseInt(rgb[3], 16)
-            ] : null;
-
-            return space.rgb.hsv(rgb);
-        });
+      return space.rgb.hsv(rgb);
     });
+  });
 }
 
 
 function init() {
+  const palettes_hsv = convert(palettes);
 
-    let palettes_hsv = convert(palettes);
+  Canvas = document.getElementById('canv');
 
-    Canvas = document.getElementById("canv");
+  console.log('initialising');
+  palette_map = new PaletteMap({
+    canvas: Canvas,
+    palettes: palettes_hsv
+  });
 
-    console.log("initialising");
-    palette_map = new PaletteMap({
-        canvas: Canvas,
-        palettes: palettes_hsv,
-    });
+  random_lines = new RandomLines({
+    canvas: Canvas,
+    palettes
+  });
 
-    random_lines = new RandomLines({
-        canvas: Canvas,
-        palettes: palettes,
-    });
+  random_arcs = new RandomArcs({
+    canvas: Canvas,
+    palettes
+  });
 
-    random_arcs = new RandomArcs({
-        canvas: Canvas,
-        palettes: palettes,
-    });
+  sand_lines = new SandLines({
+    canvas: Canvas,
+    palettes
+  });
 
-    sand_lines = new SandLines({
-        canvas: Canvas,
-        palettes: palettes,
-    });
-
-    poly = new Poly({
-        canvas: Canvas,
-        palettes: palettes_hsv,
-    });
+  poly = new Poly({
+    canvas: Canvas,
+    palettes: palettes_hsv
+  });
 }
 
 init();
 const draw = {
-    palette: palette_map.draw.bind(palette_map),
-    lines: random_lines.draw.bind(random_lines),
-    arcs: random_arcs.draw.bind(random_arcs),
-    sand: sand_lines.draw.bind(sand_lines),
-    poly: poly.draw.bind(poly),
+  palette: palette_map.draw.bind(palette_map),
+  lines: random_lines.draw.bind(random_lines),
+  arcs: random_arcs.draw.bind(random_arcs),
+  sand: sand_lines.draw.bind(sand_lines),
+  poly: poly.draw.bind(poly)
 };
 
 window.draw = draw;
