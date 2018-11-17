00194bf5e15dab94919859c0e8af85670ca7ede4
ajfisher <ajfisher.td@gmail.com>
2018-11-17 19:06:03 +1100

    test method of applying fill via a texture map

--- src/lib/scratch.js
+++ src/lib/scratch.js
@@ -7,6 +7,8 @@ import SimplexNoise from 'simplex-noise';
 
 import Drawable from './drawable.js';
 
+import { SimplexFill } from './fills';
+
 import { choose, hsvts, rand_range, weight_rnd } from './utils.js';
 
 export default class Scratch extends Drawable {
@@ -37,7 +39,10 @@ export default class Scratch extends Drawable {
 
     this.simplex = new SimplexNoise(this.seed);
 
+
     super.execute(opts);
+
+    // sf.fill(this.tx_ctx, [0,0,0]);
 
     const { ctx } = this;
     const steps = 900;
@@ -87,20 +92,61 @@ export default class Scratch extends Drawable {
     // try using quadratic curves now.
     // first make an array of points.
     const pts = [];
+    let minx = x1;
+    let miny = y3;
+    let maxx = minx;
+    let maxy = miny;
     for (let x = x1; x < x2; x = x + seg_length) {
       const y = y3 + this.simplex.noise2D(x, y3) * 30;
       pts.push({x, y});
+
+      if (x < minx) minx = x;
+      if (y < miny) miny = y;
+      if (x > maxx) maxx = x;
+      if (y > maxy) maxy = y;
     }
 
-    ctx.beginPath();
-    ctx.moveTo(pts[0].x, pts[0].y);
+    const w = this.texture.width;
+    const h = this.texture.height;
+    // create the texture fill
+    this.tx_ctx.clearRect(0,0, w, h);
+
+    const sf = new SimplexFill({
+      width: this.texture.width,
+      height: this.texture.height,
+      alpha: 1.0,
+      scale: 0.2,
+      steps: 512,
+      simplex: this.simplex
+    });
+
+    sf.fill(this.tx_ctx, [0,0,0]);
+
+    // draw the line to the predraw canvas to prep composition
+    const pd_ctx = this.pd_ctx;
+    pd_ctx.clearRect(0,0, w, h);
+
+    pd_ctx.save();
+    pd_ctx.strokeStyle = ctx.strokeStyle;
+    pd_ctx.lineWidth = ctx.lineWidth;
+    pd_ctx.beginPath();
+    pd_ctx.moveTo(pts[0].x, pts[0].y);
     let i = 0;
+
     for (i = 1; i < pts.length - 2; i++) {
       const c = (pts[i].x + pts[i+1].x) / 2;
       const d = (pts[i].y + pts[i+1].y) / 2;
-      ctx.quadraticCurveTo(pts[i].x, pts[i].y, c, d);
+      pd_ctx.quadraticCurveTo(pts[i].x, pts[i].y, c, d);
     }
-    ctx.quadraticCurveTo(pts[i].x, pts[i].y, pts[i+1].x, pts[i+1].y);
-    ctx.stroke();
+    pd_ctx.quadraticCurveTo(pts[i].x, pts[i].y, pts[i+1].x, pts[i+1].y);
+    pd_ctx.stroke();
+
+    pd_ctx.globalCompositeOperation = 'destination-out';
+    pd_ctx.drawImage(this.texture, 0, 0);
+
+    pd_ctx.restore();
+
+    // now merge back to the main canvas
+    ctx.drawImage(this.predraw, 0, 0);
   }
 }
