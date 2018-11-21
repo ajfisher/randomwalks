ff4201d5cdadd7808f09518300b5abb7ea6a3237
ajfisher <ajfisher.td@gmail.com>
2018-11-21 21:20:17 +1100

    Added connected curves drawable

--- src/lib/connected.js
+++ src/lib/connected.js
@@ -48,7 +48,7 @@ class ConnectedPass extends Actionable {
       curve_pts[c] = [s_pts[c]];
     }
 
-    const interval = 0.005;
+    const interval = 0.0005;
     const y_sd = this.tightness;
     let no_points = 1; // use this to keep track of how many joints we have
 
@@ -65,6 +65,7 @@ class ConnectedPass extends Actionable {
     }
 
     // draw each curve
+    ctx.globalAlpha = 0.5 * this.alpha;
     for (let c = 0; c < pts.length; c++) {
       ctx.beginPath();
       ctx.moveTo(curve_pts[c][0][0] * width, curve_pts[c][0][1] * height);
@@ -77,39 +78,43 @@ class ConnectedPass extends Actionable {
 
     // draw connecting lines
     // TODO turn this into sandlines between the points
-    // Abstract this out to work for any number of curves
     ctx.lineWidth = this.dot_size * width;
+    ctx.globalAlpha = this.alpha;
     for (let j = 0; j < no_points; j++) {
       ctx.beginPath();
       ctx.moveTo(curve_pts[0][j][0] * width, curve_pts[0][j][1] * height);
-      ctx.lineTo(curve_pts[1][j][0] * width, curve_pts[1][j][1] * height);
+      for (let c = 1; c < curve_pts.length; c++) {
+        ctx.lineTo(curve_pts[c][j][0] * width, curve_pts[c][j][1] * height);
+      }
       ctx.stroke();
     }
 
     /**
-    for (let t = interval; t < 1.0; t = t + interval) {
-      const pt = bspline(t, degree, pts);
-
+    ctx.globalAlpha = 1.0;
+    for (let j = 0; j < no_points; j++) {
       // get the angle made by the line from the last point to the current point
-      const xd = (pt[0] - last_pt[0]);
-      const yd = (pt[1] - last_pt[0]);
-      const r = Math.atan2(xd, yd);
+      const xd = (curve_pts[1][j][0] - curve_pts[0][j][0]);
+      const yd = (curve_pts[1][j][1] - curve_pts[0][j][1]);
+      // const r = Math.atan2(xd, yd);
 
       // move to the current point, rotate the canvas and then draw a line
-      // perpendicular to the curve
+      // from the first curve to the second
       ctx.save();
-      ctx.translate(pt[0] * width, pt[1] * height);
+      ctx.translate(curve_pts[0][j][0] * width, curve_pts[0][j][1] * height);
       // jitter the rotation
-      const noiser = this.simplex.noise2D(r, t);
-      ctx.rotate(r * noiser);
+      // const noiser = this.simplex.noise2D(r, j);
+      // ctx.rotate(r);
 
       ctx.beginPath();
       for (let d = 0; d < no_dots; d++) {
         // use nrand to plot a bunch of dots
-        const x = Math.random() * -(Math.abs(xd));
-        const y = nrand(0, y_sd);
+        const p = (d/no_dots);
+        const x = p * xd; // Math.random() * -(Math.abs(xd));
+        const y = p * yd; // nrand(0, yd);
         const noisex = this.simplex.noise2D(x, d);
         const noisey = this.simplex.noise2D(y, d);
+
+        const c = this.colours[2];
 
         // choose colour
         let c = this.colours[0];
@@ -119,17 +124,17 @@ class ConnectedPass extends Actionable {
           c = this.colours[2];
         }
 
+
         const ds = this.dot_size * width;
         ctx.moveTo(x * width, y * height);
-
         ctx.fillStyle = hsvts(c);
         ctx.arc(x * width, y* height, ds, 0, TAU);
       }
       ctx.fill();
 
-      ctx.restore();
-      last_pt = pt;
-    }**/
+        ctx.restore();
+      }
+     **/
 
     ctx.restore();
   }
@@ -177,41 +182,37 @@ export default class Connected extends Drawable {
 
     this.simplex = new SimplexNoise();
 
-    const no_points = 10; // rnd_range(4, 15);
+    const no_points = rnd_range(4, 15);
+    const no_curves = rnd_range(2, 4);
     const passes =  1; // rnd_range(20, 30);
-    const tightness = rnd_range(0.18, 0.22);
-    const curve1 = [];
-    const curve2 = [];
+    const curve_pts = [];
 
-    const line_width = 0.002;
-    const alpha = 0.5;
+    const line_width = 0.001;
+    const alpha = 0.15;
     const dot_size = 0.001;
 
-    console.log(no_points, passes, tightness);
-
+    console.log(no_points, no_curves);
 
     // create some initial starting locations.
-    for (let p = 0; p < no_points; p++) {
-      curve1.push([
-        rnd_range(0.1, 0.9),
-        rnd_range(0.1, 0.9)
-      ]);
-      curve2.push([
-        rnd_range(0.1, 0.9),
-        rnd_range(0.1, 0.9)
-      ]);
+    for (let c = 0; c < no_curves; c++) {
+      curve_pts[c] = [];
+      for (let p = 0; p < no_points; p++) {
+        curve_pts[c].push([
+          rnd_range(0.1, 0.9),
+          rnd_range(0.1, 0.9)
+        ]);
+      }
     }
 
     for (let i = 0; i < passes; i++) {
       this.enqueue(new ConnectedPass({
         alpha,
         width, height,
-        points: [curve1, curve2],
+        points: curve_pts,
         dot_size,
         line_width,
         simplex: this.simplex,
         colours: opts.fgs,
-        tightness,
         t: i
       }), opts.fg);
       // opts.fgs[i % (opts.fgs.length - 1)]);
