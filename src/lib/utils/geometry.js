fb96ad8fe331e9d6f1c095ca0688c8d6b9ece5aa
ajfisher <ajfisher.td@gmail.com>
2018-11-28 00:06:38 +1100

    Moved convex hull processor to geometry module

--- src/lib/utils/geometry.js
+++ src/lib/utils/geometry.js
@@ -1,6 +1,8 @@
 'use strict';
 // A set of geometry fucntions that are useful
 
+import { reverse } from 'lodash';
+
 export function chaikin(points) {
   // takes the set of polygon points and then uses the chaikin curve algoritm to
   // put in additional subdivisions to try and relax it into a curve.
@@ -34,3 +36,39 @@ export function chaikin(points) {
   return new_points;
 }
 
+export function convex(points) {
+  // take a list of points and then convert them to a convex hull
+  // use similar to Graham's Algorithm but we know that the points are
+  // the outer points anyway so don't need to contain any
+
+  // find the left and rightmost items
+  const xsorted = _.sortBy(points, ['x']);
+  const minpt = xsorted[0];
+  const maxpt = xsorted[xsorted.length-1];
+  // now iterate across the x positions and then add the points to the top
+  // and bottom hulls
+  const toppts = [];
+  let bottompts = [];
+
+  for (let i = 1; i < xsorted.length; i++) {
+    const curr_pt = xsorted[i];
+    if (curr_pt.y < minpt.y) {
+      // check to see if we have a major dip problem
+      if (toppts.length >= 2) {
+        const lst_pt = toppts[toppts.length-1];
+        const sndlst_pt = toppts[toppts.length-2];
+        if (lst_pt.y > curr_pt.y && sndlst_pt.y < lst_pt.y) {
+          // we have a big dip so push that point to the bottom points instead
+          bottompts.push(toppts.pop());
+        }
+      }
+      toppts.push(curr_pt);
+    } else {
+      bottompts.push(curr_pt);
+    }
+  }
+  bottompts = reverse(bottompts);
+
+  // reconstruct the array in order
+  return [minpt, ...toppts, ...bottompts];
+}
