79dd3b1791dd0a2a7206fd6f4b6dcbcafd6b4040
ajfisher <ajfisher.td@gmail.com>
2018-12-03 21:01:09 +1100

    Added circle intersection lib

--- src/lib/utils/geometry.js
+++ src/lib/utils/geometry.js
@@ -72,3 +72,41 @@ export function convex(points) {
   // reconstruct the array in order
   return [minpt, ...toppts, ...bottompts];
 }
+
+export function circle_intersections(c1, c2) {
+  // based on the maths here:
+  // http://math.stackexchange.com/a/1367732
+  // based on implementation from here:
+  // https://gist.github.com/jupdike/bfe5eb23d1c395d8a0a1a4ddd94882ac
+
+  // c1 and c2 are objects of the form {x, y, r}
+  // where x and y are the centre points and r is the radius
+  const centerdx = c1.x - c2.x;
+  const centerdy = c1.y - c2.y;
+  const R = Math.sqrt(centerdx * centerdx + centerdy * centerdy);
+  if (!(Math.abs(c1.r - c2.r) <= R && R <= c1.r + c2.r)) { // no intersection
+    return []; // empty list of results
+  }
+
+  // intersection(s) should exist
+
+  const R2 = R*R;
+  const R4 = R2*R2;
+  const a = (c1.r*c1.r - c2.r*c2.r) / (2 * R2);
+  const r2r2 = (c1.r*c1.r - c2.r*c2.r);
+  const c = Math.sqrt(2 * (c1.r*c1.r + c2.r*c2.r) / R2 - (r2r2 * r2r2) / R4 - 1);
+
+  const fx = (c1.x + c2.x) / 2 + a * (c2.x - c1.x);
+  const gx = c * (c2.y - c1.y) / 2;
+  const ix1 = fx + gx;
+  const ix2 = fx - gx;
+
+  const fy = (c1.y + c2.y) / 2 + a * (c2.y - c1.y);
+  const gy = c * (c1.x - c2.x) / 2;
+  const iy1 = fy + gy;
+  const iy2 = fy - gy;
+
+  // note if gy == 0 and gx == 0 then the circles are tangent and there is only one solution
+  // but that one solution will just be duplicated as the code is currently written
+  return [{x: ix1, y: iy1}, {x: ix2, y: iy2}];
+}
