479aa9f1c1d805158d2be5c577b723f74602932f
ajfisher <ajfisher.td@gmail.com>
2018-12-07 00:49:40 +1100

    fixed frame to take variable sizes

--- src/lib/primatives/circle_frame.js
+++ src/lib/primatives/circle_frame.js
@@ -13,18 +13,25 @@ export default class Frame extends Actionable {
 
     this.radius = opts.radius || 0.4;
     this.line_width = opts.line_width || 0.01;
+    this.extent = opts.extent || {start: 0, end: 1}; // extent of tau to draw
   }
 
   draw(ctx, colour, ...rest) {
     const {width, height, translate, radius, line_width} = this;
+    const {start, end} = this.extent;
 
     ctx.save();
     ctx.lineWidth = line_width * width;
+
+    if (end < 1.0) {
+      ctx.lineCap = 'round';
+    }
+
     ctx.strokeStyle = hsvts(colour);
     ctx.globalAlpha = this.alpha;
     ctx.translate(translate.x*width, translate.y*height);
     ctx.beginPath()
-    ctx.arc(0, 0, radius * width, 0, TAU);
+    ctx.arc(0, 0, radius * width, start * TAU, end * TAU);
     ctx.stroke();
     ctx.restore();
   }
