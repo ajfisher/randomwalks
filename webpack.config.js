061a85fc2ddd23efeb6ebc46d85203226f33359a
ajfisher <ajfisher.td@gmail.com>
2020-11-01 21:26:24 +1100

    refactor: removed all babel config and simplified

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,13 @@
 const path = require('path');
-// const nodeExternals = require('webpack-node-externals');
-
 
 module.exports = {
   context: __dirname + '/src',
 
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: [/node_modules/],
-      use: [{
-        loader: 'babel-loader',
-        options: { presets: ['env'] }
-      }]
-    }]
+  mode: 'development',
+
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
   },
 
   entry: {
@@ -22,20 +16,13 @@ module.exports = {
 
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist', 'assets'),
-    publicPath: '/assets'
+    path: path.resolve(__dirname, 'dist/assets'),
+    publicPath: '/assets/'
   },
 
-  devServer: {
-    contentBase: path.resolve(__dirname, 'src')
-  },
-  devtool: 'source-map',
-
-  node: {
-    fs: 'empty',
-    module: 'empty'
+  resolve: {
+    fallback: {
+      fs: false
+    }
   }
-
-  // target: 'node',
-  // externals: [nodeExternals()],
 };
