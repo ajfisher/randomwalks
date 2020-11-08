bfe3779768c9eecc37317176964699927873a580
ajfisher <ajfisher.td@gmail.com>
2020-11-08 17:31:19 +1100

    feat: updated circle mask to provide inverted form

--- src/lib/masks/circlemask.js
+++ src/lib/masks/circlemask.js
@@ -1,26 +1,40 @@
-'use strict';
-
 import Mask from './mask.js';
+import { TAU } from '../utils/geometry.js';
 
-const TAU = Math.PI * 2;
+/**
+ * Creates a circular {@link Mask} on the canvas for drawing
+ * @class
+ * @extends Mask
+ *
+ */
 
 export default class CircleMask extends Mask {
-  // creates a circular mask that can be drawn inside of.
-  constructor(options) {
-    const opts = options || {};
-    super (opts);
+  /**
+   * Create the circular mask.
+   * @param {MaskOptions} options - standard {@link MaskOptions} options
+   * @param {Number} options.radius - the radius of the mask from the centre
+   */
 
-    this.rotate = opts.rotate || 0;
-    this.translate = opts.translate || { x: 0.5, y: 0.5};
+  constructor(options={}) {
+    const opts = options;
+    super (opts);
 
     this.radius = opts.radius || 0.4;
   }
 
+  /**
+   * Draw a circular clipping mask to the context
+   *
+   * @param {Context2D} ctx - 2D canvas context to apply the mask to
+   *
+   */
+
   clip(ctx) {
+    const { radius, width, height, rotate, translate } = this;
     super.clip(ctx);
 
-    const xt = this.translate.x * this.width;
-    const yt = this.translate.y * this.height;
+    const xt = translate.x * width;
+    const yt = translate.y * height;
 
     // we start the path, save the context then do the translation and
     // rotation before drawing the clipping region. This allows us to do a
@@ -28,12 +42,19 @@ export default class CircleMask extends Mask {
     // in the stack, which gets passed back. At the end of this we'll have
     // an appropriately rotated clip plane but the canvas will obey standard
     // orientation rules.
-    const r = this.radius * this.width;
+    const r = radius * width;
 
     ctx.beginPath()
     ctx.save();
     ctx.translate(xt, yt);
     ctx.arc(0, 0, r, 0, TAU);
+
+    if (this.invert) {
+      // if inverted then we draw a "negative" plane across the whole canvas
+      // this uses the winding rules to create a reverse mask between the two shapes
+      ctx.rect(0.5 * width, -0.5 * height, -1 * width, height);
+    }
+
     ctx.restore();
     ctx.clip();
   }
--- src/lib/masks/mask.js
+++ src/lib/masks/mask.js
@@ -1,20 +1,48 @@
-'use strict';
+/**
+ * Abstract class definition for a Mask to define an area of
+ * the canvas to draw on and not go outside that boundary.
+ * @class
+ * @abstract
+ *
+ */
 
 export default class Mask {
-  // creates an abstract class for a Mask that can be used to define
-  // an area of the canvas to draw on and not go outside that boundary.
-  // provides the mask that is needed
-  constructor(options) {
-    // build a new mask
-    const opts = options || {};
-    this.height = opts.height;
-    this.width = opts.width;
+  /**
+   * Create the Mask
+   *
+   * @param {Object=} options - a {@link MaskOptions} object
+   *
+   * @typedef {Object} MaskOptions
+   * @prop {Number} height - height of the containing context
+   * @prop {Number} width - width of the containing context
+   * @prop {Number} rotate - amount, in radians to rotate before drawing the Mask
+   * @prop {Point} translate - the {@link Point} to translate to before drawing the mask
+   * @prop {Boolean} invert - whether to invert the mask (ie draw outside the path)
+   *
+   */
+
+  constructor(options={}) {
+    const opts = options;
+    this.height = opts.height || 10;
+    this.width = opts.width || 10;
+    this.rotate = opts.rotate || 0;
+    this.translate = opts.translate || {x: 0.5, y: 0.5};
+    this.invert = opts.invert || false;
   }
+
+  /**
+   * Draw the clipping mask to the context
+   *
+   * @param {Context} ctx - Canvas drawing context to draw to
+   *
+   */
 
   clip(ctx) {
     // abstract interface for the mask drawing actions.
     if (typeof(ctx) === 'undefined') {
       throw new Error('Clip must take a context as an argument');
     }
+
+    // TODO change this to apply translation and roation here.
   }
 }
