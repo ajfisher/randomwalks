ad944429b18bd8ce4b9b572271eb7dd179e7b31f
ajfisher <ajfisher.td@gmail.com>
2020-11-12 00:04:48 +1100

    feat: Add an actionable rectangle

--- src/lib/actions/index.js
+++ src/lib/actions/index.js
@@ -4,10 +4,12 @@ import Actionable from './actionable.js';
 import FieldGrid from './fieldgrid.js';
 import { ParticleUpdate } from '../systems/particles.js'
 import Polygon from './poly.js';
+import Rectangle from './rect.js';
 
 export {
   Actionable,
   FieldGrid,
   ParticleUpdate,
-  Polygon
+  Polygon,
+  Rectangle
 };
